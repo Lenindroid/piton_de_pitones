70f894bdc04f02df3f3cd7f964e257de0754210f
Lenen <leninrojassanchez09@gmail.com>
2024-01-13 15:39:11 -0600

    Main menu

---
+++ main.py
@@ -0,0 +1,38 @@
+import pygame
+
+# pygame setup
+pygame.init()
+screen = pygame.display.set_mode((800, 600))
+pygame.display.set_caption('Pitón de pitones')
+clock = pygame.time.Clock()
+running = True
+
+# loading assets 
+logo = pygame.image.load('assets\logo_alpha_1_0_0.png')
+play_button = pygame.image.load('assets\play_button.png')  # Reemplaza 'play_button.png' con el nombre de tu archivo de botón
+
+# buttons positioning
+button_rect = play_button.get_rect()
+button_rect.center = (400, 400)
+
+# menu functions
+
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            #I du no wat tu du
+            pass
+
+    # RENDER YOUR GAME HERE
+    screen.fill((255, 255, 255)) 
+    screen.blit(logo, (100, 100))  
+    screen.blit(play_button, button_rect) 
+    # flip() the display to put your work on screen
+    pygame.display.flip()
+
+    clock.tick(60)  # limits FPS to 60
+
+pygame.quit()
