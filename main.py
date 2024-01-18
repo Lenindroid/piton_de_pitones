5d538996ff6ef1e392994a1e695dda7ac157f3e3
Lenen <leninrojassanchez09@gmail.com>
2024-01-17 18:26:16 -0600

    Static main menu

--- main.py
+++ main.py
@@ -1,29 +1,36 @@
 import pygame
-from pygame.sprite import _Group
 
 # pygame setup
 pygame.init()
 screen = pygame.display.set_mode((800, 600))
-pygame.display.set_caption('Pitón de pitones')
+pygame.display.set_caption('Pitón de pitones (Alpha 1.0.1)')
 clock = pygame.time.Clock()
 running = True
 
-# loading assets 
-logo = pygame.image.load('assets\logo_alpha_1_0_0.png').convert_alpha()
-play_button = pygame.image.load('assets\play_button.png').convert_alpha()
-
-# buttons positioning
-button_rect = play_button.get_rect()
-button_rect.center = (400, 400)
-
-# snake
+# defining classes
 class Snake(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
         self.image = pygame.image.load('assets\snake\head.png').convert_alpha()
-        self.rect = self.image.get_rect(topleft=(400, 300))
+        self.rect = self.image.get_rect(center=(400, 300))
+
+class Static_Image (pygame.sprite.Sprite):
+    def __init__(self, route, alpha, **rectangle):
+        super().__init__()
+        if (alpha): 
+            self.image = pygame.image.load(route).convert_alpha()
+        else: 
+            self.image = pygame.image.load(route).convert()
+            
+        self.rect = self.image.get_rect(**rectangle)
+
+# loading images
+grass = Static_Image('assets\grass.png', False, topleft=(0, 0))
+logo = Static_Image('assets\logo.png', True, center=(400, 150))
+button_play = Static_Image('assets\play_button.png', True, topleft=(200, 186))
 
 
+# game loop
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -33,9 +40,9 @@ while running:
             pass
 
     # RENDER YOUR GAME HERE
-    screen.fill(('white')) 
-    screen.blit(logo, (100, 100))  
-    screen.blit(play_button, button_rect) 
+    screen.blit(grass.image, grass.rect) 
+    screen.blit(logo.image, logo.rect)
+    screen.blit(button_play.image, button_play.rect)
     # flip() the display to put your work on screen
     pygame.display.flip()
 
